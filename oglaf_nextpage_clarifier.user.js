536ab815f6409659dd06f7d04e495bc9d9392f36
AdamRGrey <adam.r.grey@gmail.com>
2014-10-15 19:36:08 -0400

    autoupdate attempt 2
    ok so downloadURL can be unspecified to allow the script to just be
    updated from wherever it was first installed. But whatever I'll do it
    anyway. maybe version is the important part.

--- oglaf_nextpage_clarifier.user.js
+++ oglaf_nextpage_clarifier.user.js
@@ -3,8 +3,9 @@
 // @namespace   https://greasyfork.org/users/2226-adam
 // @description Oglaf has stories which may have 1 or more pages. The "Next Page" button takes you to the next story if there isn't a next page, which is a bit unclear.
 // @include     htt*://oglaf.com/*
-// @version     0.1
+// @version     0.1.1
 // @grant       none
+// @downloadURL https://greasyfork.org/scripts/5750-oglaf-nextpage-clarifier/code/oglaf%20NextPage%20clarifier.user.js
 // ==/UserScript==
 
 if (!window.jQuery) 
