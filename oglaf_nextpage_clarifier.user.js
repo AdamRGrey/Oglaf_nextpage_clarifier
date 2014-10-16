ebe38a3e0eb4c21531e43ac5e5d12821b5258849
AdamRGrey <adam.r.grey@gmail.com>
2014-10-15 20:39:59 -0400

    done
    I mean in theory it'll have to be updated every time oglaf updates, but
    whatever.

--- oglaf_nextpage_clarifier.user.js
+++ oglaf_nextpage_clarifier.user.js
@@ -3,7 +3,7 @@
 // @namespace   https://greasyfork.org/users/2226-adam
 // @description Oglaf has stories which may have 1 or more pages. The "Next Page" button takes you to the next story if there isn't a next page, which is a bit unclear.
 // @include     htt*://oglaf.com/*
-// @version     0.1.3
+// @version     1.0
 // @grant       none
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
 // ==/UserScript==
@@ -14,17 +14,10 @@ function main()
 	var nextStoryLink = navlinks.children()[4];
 	if(nextPageLink.href == nextStoryLink.href)
 	{
-		alert("this story has no more pages!");
 		var NoNextPageElm = document.createElement('div');
 		NoNextPageElm.id="nnx";
 		NoNextPageElm.className="nav_ro";
 		nextPageLink.parentNode.replaceChild(NoNextPageElm, nextPageLink);
 	}
-	else
-	{
-		alert("this story has more pages!")
-	}
 }
 $(document).ready(main);
-
-//alert(navlinks);
